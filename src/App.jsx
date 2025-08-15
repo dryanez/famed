edc54a9205cf18483d7c7ad257a227c67f309f77
dryanez <dr.felipeyanez@gmail.com>
2025-08-15 17:27:49 +0200

    WORKING VERSION: Remove Layout component, keep only Dashboard and Flashcards

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import './App.css'
+import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from "./contexts/AuthContext"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Layout from "./pages/Layout"
-
-import Home from "./pages/Home"
-import Dashboard from "./pages/Dashboard"
-import Flashcards from "./pages/Flashcards"
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/flashcards" element={<Flashcards />} />
-          </Routes>
-        </Layout>
-      </Router>
+      <Pages />
       <Toaster />
     </AuthProvider>
   )
