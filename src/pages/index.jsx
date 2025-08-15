27ab78cb259832582cfab9fc2377c3fef2a11454
dryanez <dr.felipeyanez@gmail.com>
2025-08-15 13:11:02 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Layout from "../Layout";
+import Layout from "./Layout"
 import Dashboard from "./Dashboard";
 import Practice from "./Practice";
 import Progress from "./Progress";
